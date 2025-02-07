610dcecb48316d375994d4a39743b318053f72eb
NDK1195 <ndkhoa1195@gmail.com>
2025-02-07 15:08:21 +0700

    add cart ui

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,34 @@
+const items = [
+  {
+    image: {
+      thumbnail: "./assets/images/image-waffle-thumbnail.jpg",
+      mobile: "./assets/images/image-waffle-mobile.jpg",
+      tablet: "./assets/images/image-waffle-tablet.jpg",
+      desktop: "./assets/images/image-waffle-desktop.jpg",
+    },
+    name: "Waffle with Berries",
+    category: "Waffle",
+    price: 6.5,
+  },
+  {
+    image: {
+      thumbnail: "./assets/images/image-creme-brulee-thumbnail.jpg",
+      mobile: "./assets/images/image-creme-brulee-mobile.jpg",
+      tablet: "./assets/images/image-creme-brulee-tablet.jpg",
+      desktop: "./assets/images/image-creme-brulee-desktop.jpg",
+    },
+    name: "Vanilla Bean Crème Brûlée",
+    category: "Crème Brûlée",
+    price: 7.0,
+  },
+];
 function Cart() {
   return (
-    <div className="max-h-fit w-full rounded-xl bg-white p-6 xl:max-w-[384px]">
-      <h2 className="text-red-1 mb-6 text-2xl leading-none font-bold">
+    <div className="flex max-h-fit w-full flex-col gap-6 rounded-xl bg-white p-6 xl:max-w-[384px]">
+      <h2 className="text-red-1 text-2xl leading-none font-bold">
         Your Cart (0)
       </h2>
-      <div className="flex flex-col items-center gap-4 py-4">
+      {/* <div className="flex flex-col items-center gap-4 py-4">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="128"
@@ -63,7 +87,84 @@ function Cart() {
         <p className="text-rose-500-1 text-sm leading-none font-semibold">
           Your added items will appear here
         </p>
+      </div> */}
+
+      {/* cart item list */}
+      <ul className="flex flex-col">
+        {items.map((item) => (
+          <li
+            key={item.name}
+            className="border-rose-100-1 flex items-center justify-between border-b py-4 first:pt-0 last:border-none last:pb-0"
+          >
+            <div className="flex flex-col gap-3">
+              <h3 className="text-rose-900-1 text-sm leading-none font-semibold">
+                {item.name}
+              </h3>
+              <div className="flex items-center gap-2 text-sm">
+                <span className="text-red-1 leading-none font-semibold">
+                  1x
+                </span>
+                <span className="text-rose-500-1 leading-none">
+                  @&nbsp;${item.price.toFixed(2)}
+                </span>
+                <span className="text-rose-500-1 leading-none font-semibold">
+                  ${item.price.toFixed(2)}
+                </span>
+              </div>
+            </div>
+            <div className="border-rose-400-1 group hover:border-rose-900-1 flex size-5 cursor-pointer items-center justify-center rounded-full border transition-colors">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="10"
+                height="10"
+                fill="none"
+                viewBox="0 0 10 10"
+                className="text-rose-400-1 group-hover:text-rose-900-1 fill-current transition-colors"
+              >
+                <path d="M8.375 9.375 5 6 1.625 9.375l-1-1L4 5 .625 1.625l1-1L5 4 8.375.625l1 1L6 5l3.375 3.375-1 1Z" />
+              </svg>
+            </div>
+          </li>
+        ))}
+      </ul>
+      {/* cart item list */}
+
+      <hr className="border-rose-100-1" />
+
+      {/* order total */}
+      <div className="text-rose-900-1 flex items-center justify-between">
+        <p className="text-sm leading-none">Order Total</p>
+        <p className="text-2xl leading-none font-bold">$46.50</p>
       </div>
+      {/* order total */}
+      <div className="bg-rose-50-1 flex items-center justify-center gap-2 rounded-lg p-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="21"
+          height="20"
+          fill="none"
+          viewBox="0 0 21 20"
+        >
+          <path
+            fill="#1EA575"
+            d="M8 18.75H6.125V17.5H8V9.729L5.803 8.41l.644-1.072 2.196 1.318a1.256 1.256 0 0 1 .607 1.072V17.5A1.25 1.25 0 0 1 8 18.75Z"
+          />
+          <path
+            fill="#1EA575"
+            d="M14.25 18.75h-1.875a1.25 1.25 0 0 1-1.25-1.25v-6.875h3.75a2.498 2.498 0 0 0 2.488-2.747 2.594 2.594 0 0 0-2.622-2.253h-.99l-.11-.487C13.283 3.56 11.769 2.5 9.875 2.5a3.762 3.762 0 0 0-3.4 2.179l-.194.417-.54-.072A1.876 1.876 0 0 0 5.5 5a2.5 2.5 0 1 0 0 5v1.25a3.75 3.75 0 0 1 0-7.5h.05a5.019 5.019 0 0 1 4.325-2.5c2.3 0 4.182 1.236 4.845 3.125h.02a3.852 3.852 0 0 1 3.868 3.384 3.75 3.75 0 0 1-3.733 4.116h-2.5V17.5h1.875v1.25Z"
+          />
+        </svg>
+        <p className="text-rose-900-1 text-sm leading-none">
+          This is a&nbsp;<span className="font-semibold">carbon-neutral</span>
+          &nbsp;delivery
+        </p>
+      </div>
+      <button
+        className="bg-red-1 w-full cursor-pointer rounded-[999px] py-4 leading-none font-semibold text-white transition-colors hover:bg-[#952C0B]"
+        type="button"
+      >
+        Confirm Order
+      </button>
     </div>
   );
 }
